f27923148aa2ee259c7bfb7e011486e8cdadb3a1
Jimu-phoenix <youngprimemystic@gmail.com>
2025-12-13 21:28:14 +0200

    2 Teams scoreboard

--- src/pages/ScoreBoard.jsx
+++ src/pages/ScoreBoard.jsx
@@ -1,25 +1,53 @@
 import { useState } from "react";
+import Counter from '../components/Counter';
+import { useNavigate } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import '../styles/scores.css'
 
 export default function ScoreBoard(){
 
-     const [num, setNum] = useState(0);
+     const [num1, setNum1] = useState(0);
+     const [winner, setWinner] = useState(false);
+     const [num2, setNum2] = useState(0);
+     const location = useLocation();
+     const { team1, team2 } = location.state || {gameObj: {
+        team1: 'Team'
+     }};
 
-    const increase = () => {
-        setNum(num + 1);
-        console.log(num)
+    const increase = (n) => {
+        if (n === 1){
+            setNum1(num1 + 1)
+        }
+        else {
+            setNum2(num2 + 1)
+        }
+        console.log(team1)
     }
 
-    const decrease = () => {
-        setNum(num - 1);
-        console.log(num)
+    const decrease = (n) => {
+        if (n === 1){
+            setNum1(num1 - 1)
+        }
+        else {
+            setNum2(num2 - 1)
+        }
+        
     }
 
     return(
-        <section className="score">
-             <button onClick={decrease}>-</button>
 
+       <div className="container">
+         <section className="scoreboard">
+
+            
+        <section className="score1">
+
+            <h2>{`Team ${team1}`}</h2>
+             
+            <button onClick={()=>increase(1)}>+</button>
             <Counter
-                value={num}
+                value={num1}
                 places={[100, 10, 1]}
                 fontSize={80}
                 padding={5}
@@ -30,7 +58,40 @@ export default function ScoreBoard(){
                 gradientTo='transparent'
             />
 
-            <button onClick={increase}>+</button>
+        <button onClick={()=>decrease(1)}>-</button>
+            
         </section>
+
+        <hr />
+
+         <section className="score2">
+            <h2>{`Team ${team2}`}</h2>
+            <button onClick={()=>increase(2)}>+</button>
+            
+
+            <Counter
+                value={num2}
+                places={[100, 10, 1]}
+                fontSize={80}
+                padding={5}
+                gap={10}
+                textColor="white"
+                fontWeight={900}
+                gradientFrom='transparent'
+                gradientTo='transparent'
+            />
+
+             <button onClick={()=>decrease(2)}>-</button>
+        </section>
+
+        
+            
+        </section>
+
+        <button><Link to={'/'}>Done</Link></button>
+        
+
+       </div>
+
     )
 }
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,25 +1,104 @@
 import { useState } from 'react';
 import Counter from '../components/Counter';
+import { Link, useNavigate } from 'react-router-dom';
+import '../styles/Settings.css'
 
 export default function Settings(){
 
-    const [num, setNum] = useState(0);
+    
+    const [type, setType] = useState('');
+    const navigate = useNavigate();
 
-    const increase = () => {
-        setNum(num + 1);
-        console.log(num)
+
+    const changeType = (newType) => {
+        setType(newType);
     }
-
-    const decrease = () => {
-        setNum(num - 1);
-        console.log(num)
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log('Submitted')
+        console.log(e.target.Team1.value)
+        console.log(e.target.Team2.value)
+        let gameObj = {
+            team1: e.target.Team1.value,
+            team2: e.target.Team2.value
+        }
+        navigate('/scores', { state: {
+            team1: e.target.Team1.value,
+            team2: e.target.Team2.value
+        } })
     }
 
     return(
         <section className="settings">
-            <button>Back</button>
 
-            
+            <div className="top">
+                <button><Link to={'/'}>Back</Link></button>
+                <h1>Settings</h1>
+            </div>
+
+            <div className="specSet">
+
+                {/* Competition Type */}
+                <section className="compType">
+
+                    <h2>Competition Type</h2>
+                    <div className="types">
+                        <button onClick={() => changeType('2t')}>2 Teams</button>
+                        <button onClick={() => changeType('1v1')}>1 v 1</button>
+                        <button onClick={() => changeType('p2')}> 2+ Players</button>
+                    </div>
+                </section>
+
+                 <hr /> 
+
+                {/* Specific Settings */}
+                {type === '2t'? 
+                <div className="t2">
+                    <h2>Points per Round</h2>
+                    <div className="options">
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>5</button>
+                    </div>
+                    <form className="teamNames" id='teamNames' onSubmit={(e) => submitForm(e)}>
+
+                        <h3>Names</h3>
+
+                        <div className="TeamxName">
+                            <label htmlFor="Team1">Team 1</label>
+                            <input type="text" name='Team1' id='Team1'/>
+                        </div>
+
+                        <div className="TeamxName">
+                            <label htmlFor="Team2">Team 2</label>
+                            <input type="text" name='Team2' id='Team2'/>
+                        </div>
+
+                        <button type="submit" className='done'>Done</button>
+
+                    </form>
+                </div> : 
+                type === '1v1'? 
+                <div className="ovo">
+                     <h2>Points per Round</h2>
+                        <div className="options">
+                            <button>1</button>
+                            <button>2</button>
+                            <button>3</button>
+                            <button>5</button>
+                        </div>
+                </div> : 
+                <div className="p2">
+                    <h1>2+ Or More Players</h1>
+                    <h2>Coming Soon!</h2>
+                </div>
+                
+                }
+
+
+            </div>
+
            
         </section>
     )
